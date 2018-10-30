5b3dd1cdf0dea5fb04575d4bcea092ab0984e260
mridul <mriduladvani@gmail.com>
2018-10-29 20:00:17 -0400

    Updates on file server

--- clientfs.py
+++ clientfs.py
@@ -1,42 +1,33 @@
 import socket
 import argparse
-import getpass
 
-from click._compat import raw_input
-
-parser= argparse.ArgumentParser()
+parser = argparse.ArgumentParser()
 parser.add_argument('request_type', type=str, help='the type of request required from the user')
 parser.add_argument('spec', type=str, help='specific type of action that the user wants to have done ')
-parser.add_argument('-data', type=str, action='store',nargs='+', help='the data that you want to write on the file')
-parser.add_argument('-URL', type=str,help='URL of the file server')
-parser.add_argument('-p', '--port',type=int , help='the port on which the server is running and the one the client wants to connect to' )
-parser.add_argument('-d', '--path',type=str , help='The path to which the user requires the server to connect' )
-parser.add_argument('-cd', '--content_disposition', type=str, help='lets the user enter the content-disposition', default='inline')
-args=parser.parse_args()
-client_socket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address= (str(args.URL), args.port)
+parser.add_argument('-data', type=str, action='store', nargs='+', help='the data that you want to write on the file')
+parser.add_argument('-URL', type=str, help='URL of the file server')
+parser.add_argument('-p', '--port', type=int,
+                    help='the port on which the server is running and the one the client wants to connect to')
+parser.add_argument('-d', '--path', type=str, help='The path to which the user requires the server to connect')
+parser.add_argument('-cd', '--content_disposition', type=str, help='lets the user enter the content-disposition',
+                    default='inline')
+args = parser.parse_args()
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = (str(args.URL), args.port)
 client_socket.connect(server_address)
 
-
-if(args.request_type=='get'):
-    if(args.spec=='/'):
-        req_to_be_sent= args.request_type+" "+args.spec+" "+args.path
-    elif (args.spec!='/') and (args.content_disposition):
-        req_to_be_sent = args.request_type + " " + args.spec + " " + args.path+" "+args.content_disposition
+if args.request_type == 'get':
+    if args.spec == '/':
+        req_to_be_sent = args.request_type + " " + args.spec + " " + args.path
+    elif (args.spec != '/') and args.content_disposition:
+        req_to_be_sent = args.request_type + " " + args.spec + " " + args.path + " " + args.content_disposition
     else:
-        req_to_be_sent= args.request_type+" "+args.spec+" "+args.path
-
-
-
+        req_to_be_sent = args.request_type + " " + args.spec + " " + args.path
 else:
-    data="_".join(args.data)
-    req_to_be_sent= args.request_type+" "+args.spec+" "+args.path+" "+data
-
-
-
-
+    data = "_".join(args.data)
+    req_to_be_sent = args.request_type + " " + args.spec + " " + args.path + " " + data
 
 client_socket.sendall(req_to_be_sent.encode())
-response=client_socket.recv(1024)
-response=response.decode()
+response = client_socket.recv(1024)
+response = response.decode()
 print(response)
--- httpfs.py
+++ httpfs.py
@@ -2,82 +2,86 @@ import socket
 import argparse
 import os
 import magic
-HOST= '127.0.0.1'
-parser= argparse.ArgumentParser()
+
+HOST = '127.0.0.1'
+parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--debug_msgs', action='store_true', help='Enables Debugging messages')
 parser.add_argument('-p', '--port', type=int, help='thr port on which the file server connects')
-parser.add_argument('-d', '--dir_path',type=str, help='path of the directory')
-args=parser.parse_args()
-port= int(args.port)
-def_dir_path= args.dir_path
-server_address= (HOST, 65431)
-s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+parser.add_argument('-d', '--dir_path', type=str, help='path of the directory')
+args = parser.parse_args()
+port = int(args.port)
+def_dir_path = args.dir_path
+server_address = (HOST, 65431)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(server_address)
 s.listen()
-conn, adr= s.accept()
+conn, adr = s.accept()
 print('Connected by', adr)
-data= conn.recv(1024)
-data=data.decode()
-data= data.split(" ")
-req_type=data[0]
-spec=data[1].split("/")
-file= None
-def headers(content_type, content_disposition):
-    return "\n\n"+"Content-Type= "+content_type+"\nContent-Disposition= "+content_disposition
+data = conn.recv(1024)
+data = data.decode()
+data = data.split(" ")
+req_type = data[0]
+spec = data[1].split("/")
+file = None
 
-if(req_type=='get'):
-    if(spec[1]==''):
-        if(def_dir_path==data[2]):
-            arr=os.listdir(data[2])
-            response=", ".join(arr)
+
+def headers(content_type, content_disposition):
+    return "\n\n" + "Content-Type= " + content_type + "\nContent-Disposition= " + content_disposition
+
+
+if req_type == 'get':
+    if spec[1] == '':
+        if def_dir_path == data[2]:
+            arr = os.listdir(data[2])
+            response = ", ".join(arr)
         else:
-            response="HTTP Error 403: You are not authorized to access the contents of this file"
+            response = "HTTP Error 403: You are not authorized to access the contents of this file"
     else:
-        if(def_dir_path==data[2]):
-            path= data[2]+"\\"+spec[1]
+        if def_dir_path == data[2]:
+            path = data[2] + "\\" + spec[1]
             try:
                 os.path.exists(path)
-                file= open(path, "r")
-                ct= magic.from_file(path,True)
-                cd=data[3]
+                file = open(path, "r")
+                ct = magic.from_file(path, True)
+                cd = data[3]
             except IOError:
-                response="HTTP Error 404: File not found"
-                if(args.debug_msgs):
+                response = "HTTP Error 404: File not found"
+                if args.debug_msgs:
                     print("HTTP Error 404: File not found")
             else:
 
-                response=file.read()
-                if(response==''):
-                    response="HTTP Error 204: No Content in the specified file"
-                    if (args.debug_msgs):
+                response = file.read()
+                if response == '':
+                    response = "HTTP Error 204: No Content in the specified file"
+                    if args.debug_msgs:
                         print("HTTP Error 204: No Content in the specified file")
-                response=response+headers(ct,cd)
+                response = response + headers(ct, cd)
             finally:
                 if file: file.close()
         else:
-            response="HTTP Error 403: You are not authorized to access this file"
-elif(req_type=='post'):
-    if(spec[1]==''):
-        response="HTTP Error 405: Method not allowed"
-        if (args.debug_msgs):
+            response = "HTTP Error 403: You are not authorized to access this file"
+elif req_type == 'post':
+    if spec[1] == '':
+        response = "HTTP Error 405: Method not allowed"
+        if args.debug_msgs:
             print("HTTP Error 405: Method not allowed")
     else:
-        if(def_dir_path==data[2]):
-            path= data[2]+"\\"+spec[1]+".txt"
-            file= open(path, "w+")
+        if def_dir_path == data[2]:
+            path = data[2] + "\\" + spec[1]
+            file = open(path, "w+")
             try:
                 file.write(data[3])
-                response="The data has been written onto the file"
+                response = "The data has been written onto the file"
             except IOError:
-                response="HTTP Error 501: The data couldn't be written onto the file"
-                if(args.debug_msgs):
+                response = "HTTP Error 501: The data couldn't be written onto the file"
+                if args.debug_msgs:
                     print("HTTP Error 501: The data couldn't be written onto the file")
             finally:
-                if file: file.close()
-        else: response= "HTTP Error 403: You are not authorized to access the contents of the file"
+                if file:
+                    file.close()
+        else:
+            response = "HTTP Error 403: You are not authorized to access the contents of the file"
 else:
-    response="Unspecified Error"
+    response = "Unspecified Error"
 conn.sendall(response.encode())
 s.close()
-s.close()
-
